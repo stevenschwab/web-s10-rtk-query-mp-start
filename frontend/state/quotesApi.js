ff930e9b32cf8a3521b357095faea82cf9220404
Steve Schwab <schwabs@umich.edu>
2024-04-01 14:11:02 -0700

    create a quotesApi containing 2 endpoints, a getQuotes query and createQuote mutation

--- frontend/state/quotesApi.js
+++ frontend/state/quotesApi.js
@@ -1,1 +1,26 @@
-// create your RTK Query endpoints here
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const quotesApi = createApi({
+    reducerPath: 'quotesApi',
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:9009/api/quotes' }),
+    tagTypes: ['Quotes'],
+    endpoints: builder => ({
+        getQuotes: builder.query({
+            query: () => 'quotes',
+            providesTags: ['Quotes'],
+        }),
+        createQuote: builder.mutation({
+            query: (quoteAuthor, quoteText) => ({
+                url: 'quotes',
+                method: 'POST',
+                body: { quoteAuthor, quoteText },
+            }),
+            invalidatesTags: ['Quotes'],
+        }),
+    }),
+})
+
+export const {
+    useGetQuotesQuery,
+    useCreateQuoteMutation,
+} = quotesApi
